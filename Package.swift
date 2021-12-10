e2ec336c4e2fc4f39c4cbc0072c661fd86bf81eb
Larry <larrytran555@gmail.com>
2021-12-10 12:50:02 -0800

    updates swift manifest

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RouteKit",
 	platforms: [
-		.iOS(.v14)
+		.iOS(.v14),
 	],
     products: [
         .library(
@@ -25,5 +26,4 @@ let package = Package(
             dependencies: ["RouteKit"]
         ),
     ],
-    swiftLanguageVersions: [.v5]
 )
